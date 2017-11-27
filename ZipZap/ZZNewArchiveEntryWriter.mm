4662f727c6a9cfd637dcaa028818f5c314da2971
djhan <djhan@kbdmania.net>
2017-11-27 11:01:42 +0900

    fixAutoreleasing

--- ZipZap/ZZNewArchiveEntryWriter.mm
+++ ZipZap/ZZNewArchiveEntryWriter.mm
@@ -178,7 +178,7 @@ namespace ZZDataConsumer
 
 - (BOOL)writeLocalFileToChannelOutput:(id<ZZChannelOutput>)channelOutput
 					  withInitialSkip:(uint32_t)initialSkip
-								error:(out NSError**)error
+								error:(out NSError* __autoreleasing *)error //수정 발생
 {
 	ZZCentralFileHeader* centralFileHeader = [self centralFileHeader];
 	
